4d34eeb92e5427d4b46e938c7396cd9cf7a4cbd4
Vishnu <skvishnu2006@gmail.com>
2026-04-07 09:24:12 +0530

    added remaining HTML files

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import qrcode
+from datetime import datetime
 
 app = Flask(__name__)
+attendance=[]
 
 @app.route('/')
 def login():
@@ -9,18 +12,61 @@ def login():
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard1.html')
-
+  
 @app.route('/qr')
 def qr():
-    return render_template('qr.html')
+    from datetime import datetime
+    subject = "mathematics"
+    data = f"https://oncogenic-monserrate-nonenticing.ngrok-free.dev/scan?time={datetime.now().strftime('%H:%M:%S')}&subject={subject}"
+    print(data)
+    img = qrcode.make(data)
 
-@app.route('/scan')
+    path = "static/qr.png"
+    img.save(path)
+
+    return render_template('qr.html', qr_image=path,subject=subject) 
+
+from datetime import datetime
+
+@app.route('/scan', methods=['GET', 'POST'])
 def scan():
-    return render_template('scan.html')
+
+    if request.method == 'POST':
+        student_id = request.form.get('student_id')
+
+        # ✅ FIX HERE
+        subject = request.args.get('subject')
+
+        attendance.append({
+            "student": student_id,
+            "subject": subject
+        })
+
+        return render_template("success.html", student_id=student_id, subject=subject)
+
+    # GET request
+    time = request.args.get('time')
+    subject = request.args.get('subject')
+
+    return render_template('scan.html', time=time, subject=subject)
+
+@app.route('/profile')
+def profile():
+    student = {
+        "name": "VISHNUVARDHAN S",
+        "gender": "Male",
+        "dob": "14-Jun-2006",
+        "reg_no": "RA2411003020555",
+        "institution": "SRM Institute of Science and Technology",
+        "program": "B.Tech Computer Science and Engineering",
+        "semester": "III Semester"
+    }
+
+    return render_template("profile.html", student=student)
 
 @app.route('/attendance')
-def attendance():
+def view_attendance():
     return render_template('Attendance1.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
